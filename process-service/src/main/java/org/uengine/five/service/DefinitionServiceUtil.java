e081e2567a75637188b154c6fac38b0f2ec07b09
Sanghoon <sanghoon01@uengine.org>
2023-06-28 09:56:12 +0900

    update process service

--- process-service/src/main/java/org/uengine/five/service/DefinitionServiceUtil.java
+++ process-service/src/main/java/org/uengine/five/service/DefinitionServiceUtil.java
@@ -53,8 +53,11 @@ public class DefinitionServiceUtil {
 
             CloseableHttpResponse response = httpclient.execute(httpget);
             try {
-               
-                String json = "{\"value\": {\"_type\": \"org.uengine.five.service.Person\", \"name\": \"person name\", \"age\": 30, \"addresses\": [{\"_type\": \"org.uengine.five.service.Address\", \"city\":\"seoul\"},{\"_type\": \"org.uengine.five.service.GlobalAddress\", \"country\":\"Korea\", \"city\": \"yongin\"}]}}";
+                // todolist example
+                // String json = "{\"value\": {\"_type\": \"org.uengine.five.service.Person\", \"name\": \"person name\", \"age\": 30, \"addresses\": [{\"_type\": \"org.uengine.five.service.Address\", \"city\":\"seoul\"},{\"_type\": \"org.uengine.five.service.GlobalAddress\", \"country\":\"Korea\", \"city\": \"yongin\"}]}}";
+                
+                // bpm data
+                String json = "{\"_type\":\"org.uengine.kernel.ProcessDefinition\",\"processVariableDescriptors\":[{\"name\":\"var1\",\"displayName\":{\"text\":\"var1\",\"_type\":\"org.uengine.contexts.TextContext\"},\"defaultValueInString\":\"var1\",\"global\":false,\"persistOption\":\"BPMS\",\"typeClassName\":\"java.lang.String\",\"_type\":\"[Lorg.uengine.kernel.ProcessVariable\"},{\"name\":\"var2\",\"displayName\":{\"text\":\"var2\",\"_type\":\"org.uengine.contexts.TextContext\"},\"defaultValueInString\":\"var2\",\"global\":false,\"persistOption\":\"BPMS\",\"typeClassName\":\"java.lang.String\",\"_type\":\"[Lorg.uengine.kernel.ProcessVariable\"}],\"name\":\"6c22cfcddedbc88ce32b8ad3550121a4\",\"version\":3,\"scm\":{\"tag\":null,\"org\":null,\"repo\":null,\"forkedOrg\":null,\"forkedRepo\":null},\"_changedByLocaleSelector\":false,\"childActivities\":[{\"_type\":\"org.uengine.kernel.bpmn.StartEvent\",\"name\":{\"_type\":\"org.uengine.contexts.TextContext\",\"text\":\"\"},\"tracingTag\":\"1\",\"selected\":false},{\"_type\":\"org.uengine.kernel.bpmn.EndEvent\",\"name\":{\"_type\":\"org.uengine.contexts.TextContext\",\"text\":\"\"},\"tracingTag\":\"4\",\"selected\":false},{\"_type\":\"org.uengine.kernel.HumanActivity\",\"name\":{\"_type\":\"org.uengine.contexts.TextContext\",\"text\":\"task1\"},\"role\":{\"name\":null},\"parameters\":[{\"direction\":\"IN-OUT\",\"variable\":{\"name\":\"var1\"},\"argument\":{\"text\":\"arg\"}}],\"tracingTag\":\"2\",\"selected\":false,\"oldName\":\"\"},{\"_type\":\"org.uengine.kernel.HumanActivity\",\"name\":{\"_type\":\"org.uengine.contexts.TextContext\",\"text\":\"task2\"},\"role\":{\"name\":null},\"parameters\":[{\"direction\":\"IN-OUT\",\"variable\":{\"name\":\"var2\"},\"argument\":{\"text\":\"arg\"}}],\"tracingTag\":\"3\",\"selected\":false,\"oldName\":\"\"}],\"sequenceFlows\":[{\"name\":\"\",\"_type\":\"org.uengine.kernel.bpmn.SequenceFlow\",\"selected\":false,\"sourceRef\":\"1\",\"targetRef\":\"2\",\"from\":\"8690e0ac-6521-a173-8451-0c525316c714\",\"to\":\"31aaa217-3f34-7366-3422-2714f89497a1\",\"priority\":1,\"condition\":{\"_type\":\"org.uengine.kernel.Evaluate\",\"pv\":{\"_type\":\"org.uengine.kernel.ProcessVariable\",\"name\":\"\"},\"condition\":\"==\",\"val\":\"\"}},{\"name\":\"\",\"_type\":\"org.uengine.kernel.bpmn.SequenceFlow\",\"selected\":false,\"sourceRef\":\"2\",\"targetRef\":\"3\",\"from\":\"31aaa217-3f34-7366-3422-2714f89497a1\",\"to\":\"21abf127-709f-c1c2-5573-7882157048e5\",\"priority\":1,\"condition\":{\"_type\":\"org.uengine.kernel.Evaluate\",\"pv\":{\"_type\":\"org.uengine.kernel.ProcessVariable\",\"name\":\"\"},\"condition\":\"==\",\"val\":\"\"}},{\"name\":\"\",\"_type\":\"org.uengine.kernel.bpmn.SequenceFlow\",\"selected\":false,\"sourceRef\":\"3\",\"targetRef\":\"4\",\"from\":\"21abf127-709f-c1c2-5573-7882157048e5\",\"to\":\"d95a0124-9edf-2423-9627-eeba33d660ce\",\"priority\":1,\"condition\":{\"_type\":\"org.uengine.kernel.Evaluate\",\"pv\":{\"_type\":\"org.uengine.kernel.ProcessVariable\",\"name\":\"\"},\"condition\":\"==\",\"val\":\"\"}}]}";
                 ObjectMapper mapper = new ObjectMapper();
 
                 mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -65,14 +68,15 @@ public class DefinitionServiceUtil {
                 typeResolver.typeProperty("_type");
                 mapper.setDefaultTyping(typeResolver);
 
-                Root root = mapper.readValue(json, Root.class);
-
-                Person person = (Person)root.getValue();
-                System.out.println("Name: " + person.getName());
-                System.out.println("Age: " + person.getAge());
+                // Root root = mapper.readValue(json, Root.class);
+                ProcessDefinition test = mapper.readValue(json, ProcessDefinition.class);
+                
+                // Person person = (Person)root.getValue();
+                // System.out.println("Name: " + person.getName());
+                // System.out.println("Age: " + person.getAge());
         
-                System.out.println("Address1: " + person.getAddresses().get(0).getClass());
-                System.out.println("Address2: " + person.getAddresses().get(1).getClass());
+                // System.out.println("Address1: " + person.getAddresses().get(0).getClass());
+                // System.out.println("Address2: " + person.getAddresses().get(1).getClass());
 
                 String result = EntityUtils.toString(response.getEntity());
                 System.out.println(result);
